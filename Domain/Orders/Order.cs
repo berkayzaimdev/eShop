ce2de6e741f3e3c7223161f51e928c18c941a1c7
berkayzaimdev <berkayzaimdev@gmail.com>
2024-06-20 20:22:19 +0300

    Added Customer, Order and Product aggregate roots

---
+++ Domain/Orders/Order.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Customers;
+using Domain.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Orders
+{
+    public class Order
+    {
+        private readonly HashSet<LineItem> _lineItems = new ();
+        private Order()
+        {
+
+        }
+
+        public Guid Id { get; private set; }
+        public Guid CustomerId { get; private set; }
+
+        public static Order Create(Customer customer)
+        {
+            var order = new Order
+            {
+                Id = Guid.NewGuid(),
+                CustomerId = customer.Id
+            };
+
+            return order;
+        }
+
+        public void Add(Product product)
+        {
+            var lineItem = new LineItem(Guid.NewGuid(), Id, product.Id, product.Price);
+            _lineItems.Add(lineItem);
+        }
+    }
+}
---
+++ Domain/Products/Sku.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Products
+{
+    // stock keeping unit (stok numarası)
+    public record Sku
+    {
+        private const int DefaultLength = 15;
+        private Sku(string value) => Value = value; // factory metodu yazmak için private ctor kullanmak zorundayız
+
+        public string Value { get; init; }
+
+        // factory metod
+        public static Sku? Create(string value)
+        {
+            if(string.IsNullOrEmpty(value)) return null;
+
+            if(value.Length != DefaultLength) return null;
+            
+            return new Sku(value);
+        }
+    }
+}
